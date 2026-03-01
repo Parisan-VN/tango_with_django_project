558cb7d36ed9ed041334e6935863cff85f48e111
“Parisan <2964019V@student.gla.ac.uk>
2026-03-01 20:02:13 +0000

    Chapter 7 is done and tested!

--- rango/models.py
+++ rango/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    NAME_MAX_LENGTH = 128
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
     likes = models.IntegerField(default=0)
     views = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
-
+    
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
